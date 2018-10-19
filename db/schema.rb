c78319bd77f9ed1d0da7600dad4b713b7598320d
Joseph Noott <Jnootty@hotmail.com>
2018-10-19 16:50:58 +0200

    clients dropped

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007132310) do
+ActiveRecord::Schema.define(version: 20181019144938) do
 
   create_table "arcs", force: :cascade do |t|
     t.string "first_name"
@@ -45,16 +45,6 @@ ActiveRecord::Schema.define(version: 20181007132310) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.integer "number"
-    t.text "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
