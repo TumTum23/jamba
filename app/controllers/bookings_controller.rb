140ac47754d9e318a04cb923a00193464f0f3a2b
Joseph Noott <Jnootty@hotmail.com>
2018-10-29 21:13:30 +0100

    report model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  # when this is initiated it creates a condition
+  # that is turned true when both parties confirm
+  # the meeting ( around pre defined time)
+  # Which then initiates a report shared by both of them
   def new
 
   end
