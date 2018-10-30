5c3865867b199b7117e52822c3c3c99ba3d0363b
Joseph Noott <Jnootty@hotmail.com>
2018-10-30 01:18:00 +0100

    Change plans

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,3 @@
 class RatingsController < ApplicationController
+  # to become available when a booking has been created and confirmed
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,3 @@
 class ReviewsController < ApplicationController
+  # to become available when a booking has been confirmed and finished, then a user can add a review to the arc
 end
