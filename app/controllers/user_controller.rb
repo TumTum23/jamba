341001c6903fcf9f852ed3c8c1b034018d5e4ccf
Joseph Noott <Jnootty@hotmail.com>
2018-10-30 01:19:25 +0100

    Change plan again

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
   def home
+    # after booking user can write a review and give a rating out of 5 to the arc,
+    # can also view his report and the thing asociated with his viewing
   end
 end
